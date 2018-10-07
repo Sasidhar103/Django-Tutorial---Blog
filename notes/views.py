900f12ef714f37af461c00d975334ab63bc48fb1
G.Uttej Kumar Reddy <uttej60@gmail.com>
2018-10-07 23:06:03 +0530

    display post in home

--- notes/views.py
+++ notes/views.py
@@ -7,20 +7,26 @@ import json
 
 # Create your views here.
 def index(request):
+    # data = {
+    #     'page_title': 'Home',
+    #     'notes': [{
+    #         'title': 'Note 1',
+    #         'content': 'Content of the first note is seen here'
+    #     },
+    #     {
+    #         'title': 'Note 2',
+    #         'content': 'Content of the second note is seen here'
+    #     },
+    #     {
+    #         'title': 'Note 3',
+    #         'content': 'Content of the third note is seen here'
+    #     }]
+    # }
+    notes = Notes.objects.all()
+    print (notes)
     data = {
         'page_title': 'Home',
-        'notes': [{
-            'title': 'Note 1',
-            'content': 'Content of the first note is seen here'
-        },
-        {
-            'title': 'Note 2',
-            'content': 'Content of the second note is seen here'
-        },
-        {
-            'title': 'Note 3',
-            'content': 'Content of the third note is seen here'
-        }]
+        'notes': notes
     }
     return render(request, 'notes/home.html', data)
 
