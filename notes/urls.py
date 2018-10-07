0f555950a841b41491c30b5bcd2ab2c820731c1b
G.Uttej Kumar Reddy <uttej60@gmail.com>
2018-10-07 21:06:18 +0530

    added routing b/w addNote, about pages
    DO NOT SEE pycache files

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^addnote/$', views.addNote, name='addNote'),
+    url(r'^about/$', views.about, name='about')
 ]
--- notes/views.py
+++ notes/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'notes/home.html', {})
+
+def addNote(request):
+    return render(request, 'notes/addNote.html', {})
+    
+def about(request):
+    return render(request, 'notes/about.html', {})
