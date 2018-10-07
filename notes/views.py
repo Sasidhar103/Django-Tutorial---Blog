34e467a1eb73fc0c10e7dec7543723b673fac1a0
G.Uttej Kumar Reddy <uttej60@gmail.com>
2018-10-07 21:10:01 +0530

    jinja templating & routing

--- notes/views.py
+++ notes/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'notes/home.html', {})
+    return render(request, 'notes/home.html', {
+        'page_title': 'Home'
+    })
 
 def addNote(request):
-    return render(request, 'notes/addNote.html', {})
+    return render(request, 'notes/addNote.html', {
+        'page_title': 'Add Note'
+    })
     
 def about(request):
-    return render(request, 'notes/about.html', {})
+    return render(request, 'notes/about.html', {
+        'page_title': 'About'
+    })
