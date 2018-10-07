279fdabeb7792fb1f176f888c348c9f571caac00
G.Uttej Kumar Reddy <uttej60@gmail.com>
2018-10-07 21:31:12 +0530

    jinja for loop - Method 1

--- notes/views.py
+++ notes/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'notes/home.html', {
-        'page_title': 'Home'
+        'page_title': 'Home',
+        'notes': [{
+            'title': 'Note 1',
+            'content': 'Content of the first note is seen here'
+        },
+        {
+            'title': 'Note 2',
+            'content': 'Content of the second note is seen here'
+        },
+        {
+            'title': 'Note 3',
+            'content': 'Content of the third note is seen here'
+        }]
     })
 
 def addNote(request):
