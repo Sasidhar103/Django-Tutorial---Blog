1eabb12888eebcb76a0cf5775c77d888ca2ce2a6
G.Uttej Kumar Reddy <uttej60@gmail.com>
2018-10-08 06:01:22 +0530

    login and registration UI

--- blog/urls.py
+++ blog/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('notes.urls')),
-    url(r'^users', include(profiles.urls)),
+    url(r'^users', include('profiles.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
